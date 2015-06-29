500e01dc7c622ed2fdf0d1e30e9a28f9c5f228be
jacoelho <jose.coelho@linkedcare.com>
2015-06-29 15:26:31 +0100

    testing

--- lib/package_delivery_worker.ex
+++ lib/package_delivery_worker.ex
@@ -14,9 +14,33 @@ defmodule PackageDelivery.Worker do
   end
 
   def handle_cast({:deploy, options}, state) do
-    System.cmd(options)
-    Agent.update(:collector, &([options|&1]))
+    instructions = [
+      { "git", ~w(clone --depth 1 https://github.com/jacoelho/ansible-deploy.git /tmp/deploy) },
+      { "ansible-playbook", ~w(-i /tmp/deploy/inventory /tmp/deploy/playbook.yml -e env=#{options.environment} -e service=#{options.service}) },
+    ]
+
+    deploy = Enum.reduce(instructions, :ok, fn({cmd, args}, state) ->
+      if state == :ok do
+        {_output, status} = System.cmd(cmd, args)
+
+        case status do
+         0 -> :ok
+         _ -> :failure
+        end
+      else
+        :failure
+      end
+    end)
+
+    System.cmd("rm", ["-fr", "/tmp/deploy"])
+
+    case deploy do
+      :ok      -> System.cmd("curl", ~w(#{options.callback_success}))
+      :failure -> System.cmd("curl", ~w(#{options.callback_failure}))
+    end
+
     {:noreply , state}
   end
+
 
 end
